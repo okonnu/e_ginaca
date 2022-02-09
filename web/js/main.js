ab46b094492d568e6d0eb4304aab2af3de906fb3
okonu <d.okonu1@gmail.com>
2022-02-09 17:46:45 -0600

    ceff added

--- web/js/main.js
+++ web/js/main.js
@@ -344,7 +344,7 @@ function set_metrics(pload) {
             // speed
         document.getElementById("DTIME" + lab_num).innerHTML = Math.floor(payload.downtime / 60)
         document.getElementById("cases" + lab_num).innerHTML = payload.cases
-        document.getElementById("ceff" + lab_num).innerHTML = Math.round(payload.cases / (target.tstamp * (payload.target / 3600)) * 100)
+            // document.getElementById("ceff" + lab_num).innerHTML = Math.round(payload.cases / (target.tstamp * (payload.target / 3600)) * 100)
             // efficiency
 
         switch (parseInt(lab_num)) {
